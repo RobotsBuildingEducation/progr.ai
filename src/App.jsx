8fd61fb6d29d202aa36d3b973210a63ce884ded7
RobotsBuildingEducation <robotsbuildingeducation@gmail.com>
2024-07-21 12:38:01 -0700

    add chakra ui

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,19 @@
 import { useState } from "react";
+import { Button, Box, Text, Image } from "@chakra-ui/react";
 import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
 
 function App() {
   const [count, setCount] = useState(0);
 
-  return <main>Hello world</main>;
+  return (
+    <Box textAlign="center" fontSize="xl">
+      <Text>Hello world</Text>
+      <Button colorScheme="teal" onClick={() => setCount(count + 1)} mt={4}>
+        Count is {count}
+      </Button>
+    </Box>
+  );
 }
 
 export default App;
